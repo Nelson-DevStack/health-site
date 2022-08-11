c3c3b0be69604530b451323bbf0db06f362c3e85
Nelson C <n.devpro@protonmail.com>
2022-08-10 21:56:21 -0400

    hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 
 import Main from '../components/Main';
@@ -7,6 +8,7 @@ import Navbar from '../components/Navbar';
 import Container from '../components/UI/Container';
 import Heading from '../components/UI/Heading';
 import Text from '../components/UI/Text';
+import { mainColor } from '../utils/colors';
 
 const Home: NextPage = () => {
   return (
@@ -19,16 +21,24 @@ const Home: NextPage = () => {
 
       <Navbar />
 
-      <Main>
-        <Container>
-          <Heading>Quer uma Alimentação Saudável?</Heading>
-          <Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-            deserunt repellendus ipsam doloremque excepturi aperiam rerum harum
-            animi laborum, consequatur aut possimus sequi perferendis iusto
-            dolor iure laboriosam id beatae.
-          </Text>
-          <p className="font-heading">abc</p>
+      <Main className={`bg-[url('/hero-image1.jpg')] bg-cover py-32 md:py-40`}>
+        <Container className="grid md:grid-cols-2">
+          <div className="bg-white/80  md:bg-transparent rounded-lg p-5 md:p-0">
+            <Heading>
+              Quer uma{' '}
+              <span style={{ color: mainColor }}>Alimentação Saudável</span>?
+            </Heading>
+            <Text>
+              Conheça nossas receitas e dicas para ser cada vez mais saudável.
+            </Text>
+
+            <button
+              type="button"
+              className={`p-2 px-5 mt-4 rounded-sm text-slate-100 bg-[${mainColor}]`}
+            >
+              <Link href="/">Conhecer</Link>
+            </button>
+          </div>
         </Container>
       </Main>
     </>
