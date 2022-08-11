ac7b52e0fd5cb829cd502fc07723b1c345c97e17
Nelson C <n.devpro@protonmail.com>
2022-08-11 11:09:32 -0400

    hero md

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,9 +21,11 @@ const Home: NextPage = () => {
 
       <Navbar />
 
-      <Main className={`bg-[url('/hero-image1.jpg')] bg-cover py-32 md:py-40`}>
-        <Container className="grid md:grid-cols-2">
-          <div className="bg-white/80  md:bg-transparent rounded-lg p-5 md:p-0">
+      {/* <Main
+        className={`bg-[url('/hero-image1.jpg')] bg-cover bg-center md:bg-top py-32 md:py-40`}
+      >
+        <Container className="grid md:grid-cols-2 relative">
+          <div className="absolute -top-28 w-full bg-white md:bg-transparent rounded-lg p-5 md:p-0">
             <Heading>
               Quer uma{' '}
               <span style={{ color: mainColor }}>Alimentação Saudável</span>?
@@ -40,6 +42,33 @@ const Home: NextPage = () => {
             </button>
           </div>
         </Container>
+      </Main> */}
+
+      <Main className="">
+        <section className="md:grid md:grid-cols-2 md:relative md:h-96">
+          {/* <div className="bg-[url('/hero-image1.jpg')] md:order-2 w-full  h-52 md:h-auto bg-cover bg-center" /> */}
+          <div className="bg-[url('/hero-image1.jpg')] md:order-2 w-full h-52 md:h-auto bg-cover bg-center md:absolute md:w-full inset-0" />
+          <Container className="md:col-span-1 relative md:flex md:items-center">
+            {/* <div className="w-full md:w-auto bg-white md:order-1 md:bg-transparent rounded-lg p-5"> */}
+            <div className="w-full md:max-w-lg md:w-auto bg-white md:order-1 md:bg-transparent p-5">
+              <Heading>
+                Procura uma{' '}
+                <span style={{ color: mainColor }}>Alimentação Saudável</span>?
+              </Heading>
+              <Text className="md:max-w-sm">
+                Conheça nossas receitas e dicas para ser cada vez mais saudável.
+              </Text>
+
+              <button
+                type="button"
+                // className="p-2 px-5 mt-4 rounded-sm text-slate-100 bg-[#F44444]"
+                className={`p-2 px-5 mt-4 rounded-sm text-slate-100 bg-[${mainColor}]`}
+              >
+                <Link href="/">Conhecer</Link>
+              </button>
+            </div>
+          </Container>
+        </section>
       </Main>
     </>
   );
