9e0ed652434ec1f8ef5110d780354fc103d8e0bd
Nelson C <n.devpro@protonmail.com>
2022-08-15 10:47:09 -0400

    responsive navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
+/* eslint-disable react/jsx-curly-brace-presence */
+import React, { useState } from 'react';
+import { HiMenuAlt3, HiX } from 'react-icons/hi';
 import { Link } from 'react-scroll';
 
 import LogoLink from '../LogoLink';
 import Container from '../UI/Container';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header className="bg-white w-full h-16 col-span-4 md:col-span-8 lg:col-span-12 z-[999] shadow-md z-100 fixed">
       <Container>
-        <nav className="w-full flex h-full justify-between items-center">
+        <nav className="w-full flex h-full justify-between items-center ">
           <LogoLink />
 
-          <ul className="flex space-x-6">
+          <button
+            type="button"
+            className={`text-2xl flex items-center justify-center transition duration-500 transition-100 max-h-0 ${
+              showMenu ? 'max-h-0' : 'max-h-10'
+            }`}
+            onClick={handleMenuClick}
+          >
+            {showMenu ? <HiX /> : <HiMenuAlt3 />}
+          </button>
+
+          <ul
+            className={`flex flex-col items-center pt-12 gap-5 w-full bg-white/75 absolute min-h-screen min-w-screen left-0 right-0 top-16 shadow-md text-xl backdrop-blur-lg translate-x-full transition duration-500 ${
+              showMenu ? 'translate-x-0' : ''
+            }`}
+          >
             <li>
               <Link
                 to="home"
